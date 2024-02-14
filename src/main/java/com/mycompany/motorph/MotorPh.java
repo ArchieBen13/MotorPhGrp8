0be7e4cf0d9c86e1a1e41e4becb654a2820de748
UERM <UERM@IT-LTP03>
2024-02-14 15:27:16 +0800

    Updated Employee Details

---
+++ src/main/java/com/mycompany/motorph/MotorPh.java
@@ -0,0 +1,34 @@
+
+package com.mycompany.motorph;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
+/**
+ *
+ * @author UERM
+ */
+public class MotorPh {
+
+public static void main(String[] args) {
+    try {
+      File myObj = new File("C:\\Users\\UERM\\Documents\\NetBeansProjects\\MotorPhEmployee\\employees.csv");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        String[] cols = data.split("\",\"", 0);
+        for (String col : cols)
+            System.out.println(col);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
